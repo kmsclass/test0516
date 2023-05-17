cf78a006a7af92da7a2667d5d7434eba0dbfd24c
kms <kms.goodee.co.kr>
2023-05-17 09:02:15 +0900

    20230517-0902

---
+++ src/main/java/test/ConfigMain.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigMain {
+	public static void main(String[] args) {
+		  ApplicationContext ctx = 
+				   new AnnotationConfigApplicationContext(AppCtx.class);
+		WriteArticleService articleService = 
+				ctx.getBean("writeArticleService", WriteArticleService.class);
+		articleService.write(new Article());
+	}
+}
---
+++ src/main/java/test/WriteArticleService.java
@@ -0,0 +1,5 @@
+package test;
+
+public interface WriteArticleService {
+	void write(Article article);
+}
