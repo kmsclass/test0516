232e7335e6182c0c24b3796cde08754e348331d9
kms <kms.goodee.co.kr>
2023-05-17 09:15:05 +0900

    20230517-0915

--- src/main/java/test/AppCtx.java
+++ src/main/java/test/AppCtx.java
@@ -1,9 +1,20 @@
 package test;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration 
-@ComponentScan(basePackages = {"test"}) 
+//@ComponentScan(basePackages = {"test"}) 
 public class AppCtx {
+	@Bean
+	public WriteArticleService writeArticleService() {
+		WriteArticleService obj = new WriteArticleServiceImpl(articleDao());
+		return obj;
+	}
+	@Bean
+	public ArticleDao articleDao() {
+		 ArticleDao obj = new MariadbArticleDao();
+		 return obj;
+	}
 }
--- src/main/java/test/ConfigMain.java
+++ src/main/java/test/ConfigMain.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class ConfigMain {
 	public static void main(String[] args) {
-		  ApplicationContext ctx = 
+		ApplicationContext ctx = 
 				   new AnnotationConfigApplicationContext(AppCtx.class);
 		WriteArticleService articleService = 
 				ctx.getBean("writeArticleService", WriteArticleService.class);
